d84f80e073cf72f25f891baac1064744b2e4dc98
lwj <874279721@qq.com>
2018-10-10 14:06:58 +0800

    feat: 添加通过IntersectionObserver api的方式来判断元素是否可见

--- js/lazy-image.js
+++ js/lazy-image.js
@@ -1,25 +1,46 @@
 class LazyImage {
     constructor(selector) {
       // 懒记载图片列表，将伪数组转为数组，以便可以使用数组的api
-      this.imageElements = Array.prototype.slice.call(document.querySelectorAll(selector))
+      this.lazyImages = Array.prototype.slice.call(document.querySelectorAll(selector))
       this.init()
     }
   
     inViewShow() {
-      let len = this.imageElements.length
-      for(let i = 0; i < len; i++) {
-        let imageElement = this.imageElements[i]
-        const rect = imageElement.getBoundingClientRect()
-        // 出现在视野的时候加载图片
-        if(rect.top < document.documentElement.clientHeight) {
-          imageElement.src = imageElement.dataset.src
-          // 移除掉已经显示的
-          this.imageElements.splice(i, 1)
-          len--
-          i--
-          if(this.imageElements.length === 0) {
-            // 如果全部都加载完 则去掉滚动事件监听
-            document.removeEventListener('scroll', this._throttleFn)
+      // 通过IntersectionObserver api判断图片是否出现在可视区域内
+      if ("IntersectionObserver" in window) {
+        let lazyImageObserver = new IntersectionObserver((entries, observer) => {
+          entries.forEach((entry, index) => {
+            // 如果图片元素可见
+            if (entry.isIntersecting) {
+              let lazyImage = entry.target
+              lazyImage.src = lazyImage.dataset.src
+              lazyImage.classList.remove("lazy-image")
+              lazyImageObserver.unobserve(lazyImage)
+              this.lazyImages.splice(index, 1)
+            }
+          });
+        });
+    
+        this.lazyImages.forEach(function(lazyImage) {
+          lazyImageObserver.observe(lazyImage);
+        });
+      } else {
+        // 不支持IntersectionObserver api的情况下判断图片是否出现在可视区域内
+        let len = this.lazyImages.length
+        for(let i = 0; i < len; i++) {
+          let lazyImage = this.lazyImages[i]
+          const rect = lazyImage.getBoundingClientRect()
+          // 出现在视野的时候加载图片
+          if(rect.top < document.documentElement.clientHeight) {
+            lazyImage.src = lazyImage.dataset.src
+            // 移除掉已经显示的
+            this.lazyImages.splice(i, 1)
+            len--
+            i--
+            if(this.lazyImages.length === 0) {
+              // 如果全部都加载完 则去掉滚动事件监听
+              document.removeEventListener('scroll', this._throttleFn)
+            }
           }
         }
       }
